c2f8b2993578b19bcfb5555d64c2ca36e3cd9302
Lnx <46533182+lnxAv@users.noreply.github.com>
2024-11-27 20:43:11 -0500

    replace exemple by something more readable

--- src/api/routes/v1/auth/auth.index.ts
+++ src/api/routes/v1/auth/auth.index.ts
@@ -1,8 +1,10 @@
 import { createV1Router } from "../createApp.ts"
 import { databaseAgentMiddleware } from "../../../middleware/mongoAgent.middleware.ts"
+import { authMiddleware } from "../../../middleware/authAgent.middleware.ts"
 
 const router = createV1Router()
 .use(databaseAgentMiddleware)
+.use(authMiddleware)
 
 
 export default router
