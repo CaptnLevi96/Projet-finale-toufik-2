6bbc5df9a95c643d35c1330e634b999008380045
Lnx <46533182+lnxAv@users.noreply.github.com>
2024-11-27 19:57:02 -0500

    add message route

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -2,11 +2,13 @@
 import createV1App from './createApp.ts'
 import userRouter from './users/users.index.ts'
 import authRouter from './auth/auth.index.ts'
+import messagesRouter from './messages/messages.index.ts'
 
 // @ROUTE /v1/..
-const v1 = createV1App().
-route('/v1', userRouter)
+const v1 = createV1App()
+.route('/v1', userRouter)
 .route('/v1', authRouter)
+.route('/v1', messagesRouter)
 .get('/v1', (c) => {
     return c.redirect('/v1/ui')
 })
