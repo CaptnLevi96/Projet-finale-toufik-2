89a006b8bed6d180de8cfde573fb1b8a66b65a35
Lnx <46533182+lnxAv@users.noreply.github.com>
2024-12-01 14:47:04 -0500

    init mix

--- src/api/routes/v1/comments/comments.index.ts
+++ src/api/routes/v1/comments/comments.index.ts
@@ -8,5 +8,5 @@ const router = createV1Router()
     .openapi(routes.read, handlers.read)
     .openapi(routes.create, handlers.create)
     .openapi(routes.remove, handlers.remove)
-    
+
 export default router
